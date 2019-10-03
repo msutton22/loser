500900c2e3f0c17c324799fe271b1c03baced5cd
Maddy <ms9642@nyu.edu>
2019-10-03 16:03:58 -0400

    keys change initial code done

--- Assets/Scripts/playerBehavior.cs
+++ Assets/Scripts/playerBehavior.cs
@@ -7,30 +7,44 @@ public class playerBehavior : MonoBehaviour
     public float speed;
 
     public bool isJumping;
+    public bool newRoom;
+
+    private KeyCode right;
+    private KeyCode left;
+    private KeyCode jump;
     // Start is called before the first frame update
     void Start()
     {
-        
+        right = KeyCode.RightArrow;
+        left = KeyCode.LeftArrow;
+        jump = KeyCode.Space;
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(right))
         {
             this.GetComponent<Transform>().Translate(new Vector3(speed, 0));
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(left))
         {
             this.GetComponent<Transform>().Translate(new Vector3(-speed, 0));
         } 
-        if (!isJumping && Input.GetKeyDown(KeyCode.Space))
+        if (!isJumping && Input.GetKeyDown(jump))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,7f), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,6f), ForceMode2D.Impulse);
             isJumping = true;
 
-        } 
+        }
+
+        if (newRoom)
+        {
+            right = KeyCode.D;
+            left = KeyCode.A;
+            jump = KeyCode.W;
+        }
     }
     
     void OnCollisionEnter2D (Collision2D col)
@@ -39,9 +53,9 @@ public class playerBehavior : MonoBehaviour
         {
             isJumping = false;
         }
-        else
+        if (col.gameObject.tag.Equals("wall"))
         {
-            isJumping = true;
+            newRoom = true;
         }
     }
 }
